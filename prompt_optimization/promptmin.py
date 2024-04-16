66fad64d0a39e43e6f3a80961fef97692f709219
Avi Schwarzschild <schwarzschild@cmu.edu>
2024-04-16 13:43:38 -0400

    bugfix.

--- prompt_optimization/promptmin.py
+++ prompt_optimization/promptmin.py
@@ -31,7 +31,7 @@ def minimize_prompt(model, tokenizer, input_str, target_str, system_prompt, chat
                                                                                                   device)
         if running_max == -1:
             running_max = (target_slice.stop - target_slice.start) * 5
-        elif optimization_args["discrete_optimizer"] == "gcg":
+        if optimization_args["discrete_optimizer"] == "gcg":
             solution = prompt_opt.optimize_gcg(model, input_ids, input_slice, free_token_slice, target_slice,
                                                loss_slice, optimization_args["num_steps"],
                                                batch_size=optimization_args["batch_size"],
